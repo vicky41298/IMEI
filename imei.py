fc929573ef7f152365c3a36754cac8daacca28ee
vicky41298 <vicky41298@gmail.com>
2019-09-23 22:25:28 +0530

    Create imei.py

---
+++ imei.py
@@ -0,0 +1,62 @@
+imei=input()[:15]
+if len(imei)<15:
+  print("Please enter a valid IMEI")
+else:
+  n=0
+  do=[]
+  si=[]
+  temp=0
+  temp1=0
+  temptot=0
+  for i in imei.split():
+
+    if i.isdigit():
+      for j in range(len(i),0,-2):
+        print("j",j)
+        n=j*2
+        print("n",n)
+        do.append(n)
+        print("first do",do)
+      for k in range(1,len(i),2):
+        si.append(k)
+  print("si",si)
+  for i in (do):
+
+    if i>9:
+      print("i",i)
+      k=do.index(i)
+   
+      temp=i%10
+    
+      i=i//10
+    
+      temp1=i
+    
+      temptot=temp+temp1
+    
+      do[k]=temptot
+  print("do",do)
+  vali=sum(do)
+  vali1=sum(si)
+  kal=vali+vali1
+  print(kal)
+  if kal%10==0:
+    print("Valid one!")
+  else:
+    print("OOPS, Not a valid one!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
