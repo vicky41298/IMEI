f9373ee38b8ba7b72a10c73389c582ade8856f13
vicky41298 <vicky41298@gmail.com>
2019-09-25 21:06:49 +0530

    Update imei.py

--- imei.py
+++ imei.py
@@ -11,14 +11,15 @@ else:
   for i in imei.split():
 
     if i.isdigit():
-      for j in range(len(i),0,-2):
-        print("j",j)
-        n=j*2
+      for j in range(len(i)-1,0,-2):
+        #print("j",int(imei[j]))
+        n=int(imei[j])*2
         print("n",n)
         do.append(n)
         print("first do",do)
-      for k in range(1,len(i),2):
-        si.append(k)
+      for k in range(0,len(i)-1,2):
+        o=imei(k)
+        si.append(o)
   print("si",si)
   for i in (do):
 
@@ -44,7 +45,6 @@ else:
     print("Valid one!")
   else:
     print("OOPS, Not a valid one!")
-
 
 
 
